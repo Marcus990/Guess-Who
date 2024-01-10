153a7dd8cc5ae63480c894a404ff2d78f7354f02
kevinw2006 <153537375+kevinw2006@users.noreply.github.com>
2024-01-10 11:58:24 -0500

    Update GuessWho.java

--- GuessWho.java
+++ GuessWho.java
@@ -15,6 +15,12 @@ import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -66,6 +72,7 @@ public class GuessWho{
 	static JLabel guessWhoLogoInitial = new JLabel();
 	static JLabel rightPersonPortrait = new JLabel();
 	static JLabel leftPersonPortrait = new JLabel();
+	static JLabel enterCorrectChar = new JLabel("If the Ai guessed wrong, enter your character");
 	static JComboBox questions;
 	static JTextArea answer = new JTextArea("Insert your answer here"); 
 	
@@ -114,14 +121,27 @@ public class GuessWho{
 	static ImageIcon Liz = new ImageIcon("IMG_3773.jpg");
 	static ImageIcon Katie = new ImageIcon("IMG_3775.jpg");
 	static ImageIcon Farah = new ImageIcon("IMG_3780.jpg");
-	static ImageIcon PlaidBackground = new ImageIcon("OfficialPlaidBackground.png");
+	static ImageIcon PlaidBackground = new ImageIcon("OfficialPlaidBackground (1).png");
 	static ImageIcon GuessWhoLogo = new ImageIcon("GuessWhoLogo.png");
 	static ImageIcon RightPersonPortrait = new ImageIcon("RightPersonPortrait.png");
 	static ImageIcon LeftPersonPortrait = new ImageIcon("LeftPersonPortrait.png");
 	static ImageIcon settingsImage = new ImageIcon("Settings.png");
 	
+	static File backgroundMusicPath = new File("GuessWhoMusic.wav"); 
+	private static Clip backgroundMusic; 
+	
 	//Main method
-	public static void main(String[] args) throws IOException, FontFormatException{
+	public static void main(String[] args) throws IOException, FontFormatException, UnsupportedAudioFileException, LineUnavailableException{
+		
+		//Initiate background music 
+		AudioInputStream audioInput = AudioSystem.getAudioInputStream(backgroundMusicPath);
+		backgroundMusic = AudioSystem.getClip(); 
+		backgroundMusic.open(audioInput);
+		
+		FloatControl gainControl = (FloatControl) backgroundMusic.getControl(FloatControl.Type.MASTER_GAIN);
+		gainControl.setValue(-10.0f);
+		
+		playMusic(backgroundMusic);
 		
 		//Initiate custom font
 		Font font = Font.createFont(Font.TRUETYPE_FONT, new File("GuessWhoFont.otf")).deriveFont(20f);
@@ -166,6 +186,7 @@ public class GuessWho{
 		settings.setBorderPainted(false);
 		settings.setBorder(null);
 		settings.setFocusPainted(false);
+		settings.addActionListener(null);
 		window.add(settings);
 		
 		//Set properties for the credits JLabel on the main menu
@@ -444,6 +465,23 @@ public class GuessWho{
 		window.repaint();
 	}
 	
+	public static void playMusic(Clip clip) {
+		
+		clip.start(); 
+	
+	}
+	
+	public static void loopMusic(Clip clip) {
+		
+		clip.loop(clip.LOOP_CONTINUOUSLY);
+
+	}
+	
+	public static void stopMusic(Clip clip) {
+		
+		clip.stop();
+		
+	}
 	public static void aiSelectsQuestion() {
 		
 		int propertyCount[] = new int[25];
